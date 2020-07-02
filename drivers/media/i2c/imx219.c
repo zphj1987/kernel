cc823aab4b127b66be77f53f8902a8bd47f9d944
brian <brian@vamrs.com>
2020-07-02 19:02:01 +0800

    add imx219 power down gpio control
    Signed-off-by: brian <brian@vamrs.com>

--- drivers/media/i2c/imx219.c
+++ drivers/media/i2c/imx219.c
@@ -16,6 +16,7 @@
 #include <linux/of_graph.h>
 #include <linux/slab.h>
 #include <linux/videodev2.h>
+#include <linux/gpio/consumer.h>
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-fwnode.h>
@@ -215,6 +216,7 @@ struct imx219 {
 	struct media_pad pad;
 	struct v4l2_ctrl_handler ctrl_handler;
 	struct clk *clk;
+	struct gpio_desc *pwdn_gpio;
 	struct v4l2_rect crop_rect;
 	int hflip;
 	int vflip;
@@ -410,10 +412,17 @@ static int imx219_s_power(struct v4l2_subdev *sd, int on)
 
 	if (on)	{
 		dev_dbg(&client->dev, "imx219 power on\n");
+		if(!IS_ERR(priv->pwdn_gpio)) {
+			gpiod_set_value_cansleep(priv->pwdn_gpio, 1);
+			msleep(10);
+		}
 		clk_prepare_enable(priv->clk);
 	} else if (!on) {
 		dev_dbg(&client->dev, "imx219 power off\n");
 		clk_disable_unprepare(priv->clk);
+		if(!IS_ERR(priv->pwdn_gpio)) {
+			gpiod_set_value_cansleep(priv->pwdn_gpio, 0);
+		}
 	}
 
 	return 0;
@@ -893,6 +902,12 @@ static int imx219_probe(struct i2c_client *client,
 		return -EPROBE_DEFER;
 	}
 
+	priv->pwdn_gpio = devm_gpiod_get(&client->dev, "pwdn", GPIOD_OUT_HIGH);
+	if (IS_ERR(priv->pwdn_gpio))
+		dev_info(&client->dev, "Failed to get pwdn-gpios\n");
+
+	gpiod_set_value_cansleep(priv->pwdn_gpio, 1);
+	msleep(5);
 	/* 1920 * 1080 by default */
 	priv->cur_mode = &supported_modes[0];
 
@@ -904,22 +919,29 @@ static int imx219_probe(struct i2c_client *client,
 	v4l2_i2c_subdev_init(&priv->subdev, client, &imx219_subdev_ops);
 	ret = imx219_ctrls_init(&priv->subdev);
 	if (ret < 0)
-		return ret;
+		goto err_power_off;
 	ret = imx219_video_probe(client);
 	if (ret < 0)
-		return ret;
+		goto err_power_off;
 
 	priv->subdev.flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 	priv->pad.flags = MEDIA_PAD_FL_SOURCE;
 	priv->subdev.entity.type = MEDIA_ENT_T_V4L2_SUBDEV_SENSOR;
 	ret = media_entity_init(&priv->subdev.entity, 1, &priv->pad, 0);
 	if (ret < 0)
-		return ret;
+		goto err_power_off;
 
 	ret = v4l2_async_register_subdev(&priv->subdev);
 	if (ret < 0)
-		return ret;
+		goto err_power_off;
 
+	return ret;
+
+err_power_off:
+	if (!IS_ERR(priv->pwdn_gpio)) {
+		gpiod_set_value_cansleep(priv->pwdn_gpio, 0);
+		gpiod_put(priv->pwdn_gpio);
+	}
 	return ret;
 }
 
